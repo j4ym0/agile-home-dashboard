53cfef7094b22d3ff11fb8268b151a9753abbf0b
j4ym0 <jay_morrall@hotmail.com>
2025-10-02 13:47:34 +0100

    smartlife not configured correctly message

--- web/controller/smartlife.php
+++ web/controller/smartlife.php
@@ -1,7 +1,16 @@
 <?php
 
-$template->assign('tuya_configured', $settings->get('tuya_configured', ''));
 
 
-$tuya = new Tuya($db, $settings);
-$tuya->getDeviceList();
+try{
+    // check if configured
+    if (!$settings->get('tuya_configured')){
+        throw new Exception('Not configured');
+    }
+
+    // check tuya is working
+    $tuya = new Tuya($db, $settings);
+
+} catch (Exception $e){
+    $template->assign('tuya_config_error', true);
+}
