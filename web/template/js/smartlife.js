add205c1a8059ce38422d3cd9eabc9d3ad5b9f58
j4ym0 <jay_morrall@hotmail.com>
2025-10-16 16:05:07 +0100

    fix offline energy usage

--- web/template/js/smartlife.js
+++ web/template/js/smartlife.js
@@ -2,9 +2,10 @@ let refreshTimer = null;
 let refreshInterval = 30;
 function getCurrentPower(device){
     w = '';
+    if (!device.online) return 'Offline';
     device.status.forEach((status, index) => {
         if (status.code === 'cur_power'){
-            w = (status.value / 10);
+            w = device.status[0].value ? (status.value / 10) : 0;
         }
     });
     if (isNumber(w)){
@@ -28,7 +29,7 @@ async function getDeviceList(){
             html = '';
             result.devices.forEach((device, index) => {
                 html += '<div class="device-card prevent-select' + (device.online ? '' : ' disabled') + '" id="' + device.id + '">' + 
-                    '<div class="header"><a href="?device_id=' + device.id + '">' + device.name.trim() + (device.online ? '' : ' - Offline') + '</a></div>' + 
+                    '<div class="header"><a href="?device_id=' + device.id + '">' + device.name.trim() + '</a></div>' + 
                     '<div class="icon"><img src="' + device.icon + '"></div>'+ 
                     '<div class="status">' + getCurrentPower(device) + '</div>'+ 
                     '<div class="switch"><label class="toggle-container"><input type="checkbox" name="' + device.id + '_switch" id="' + device.id + '_switch" class="toggle-checkbox switch-checkbox" data-endpoint="/api/tuya/set_switch_status" data-id="' + device.id + '" ' + (device.status[0].value ? 'checked' : '') + '' + (device.online ? '' : ' disabled') + '><span class="toggle-track"></span></label></div>'+ 
