d9137e827f2a19b193de17ed3e0b73c78041edf6
j4ym0 <jay_morrall@hotmail.com>
2026-09-14 11:18:42 +0100

    get 2 dats of data for consumption as its a day behind

--- service/octopus.py
+++ service/octopus.py
@@ -227,7 +227,7 @@ class Octopus:
         if intervalStart == '':
             intervalStart = f"{datetime.now().strftime("%Y-%m-%d")} 00:00:00"
         if intervalEnd == '':
-            intervalEnd = f"{(datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")} 23:59:59"
+            intervalEnd = f"{(datetime.now() + timedelta(days=2)).strftime("%Y-%m-%d")} 23:59:59"
 
         # Convert the datetime string to the ISO 8601 format
         intervalStart = tools.parse_datetime(intervalStart, Config.get('app.timezone', 'UTC'))
